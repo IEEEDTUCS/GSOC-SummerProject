d92b458932b79120567c5472cb02adf5ce4f589e
Abhimanyu Kumar Jha <135356277+AbhimanyuKumarJha@users.noreply.github.com>
2024-07-12 07:15:14 +0000

    file structures

---
+++ src/components/Company-List/company.box.component.jsx
@@ -0,0 +1,3 @@
+// isme comapny card wala component aayega 
+// use props for company name, desc, and other features, sath me isse clickable banana hai  jo ki  redirect kare dashboard pe
+
---
+++ src/components/layout/header.jsx
@@ -0,0 +1,2 @@
+// header banana hai as a component and then make it position fixed  on top
+// comopent name - <Header> 
