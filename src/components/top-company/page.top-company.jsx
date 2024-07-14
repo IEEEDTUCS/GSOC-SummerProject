5f83a4fe27fa4ae5dbcdafc5b46b61719b42ae3b
Sangam Koundal <koundalsangam314@gmail.com>
2024-07-15 01:02:19 +0530

    made the page responive

--- src/components/top-company/page.top-company.jsx
+++ src/components/top-company/page.top-company.jsx
@@ -9,13 +9,14 @@ export default function TopCompany(){
     return(
         <div>
             <div className="flex flex-col items-center justify-cente ">
-                <h1 className="text-[64px] font-semibold  inline-block text-transparent bg-clip-text bg-gradient-to-b from-white via-slate-200   to-slate-600">Top Companies</h1>
+
+                <p className="lg:text-6xl md:text-5xl sm:text-4xl xs:text-3xl  font-semibold  inline-block text-transparent bg-clip-text bg-gradient-to-b from-white via-slate-200 to-slate-600 mt-3">Top Companies</p>
 
 
                 
-                <button className="w-[150px] h-[54px] border-[#282D45] border-2 rounded-[46px] flex justify-center items-center bg-gradient-to-r from-slate-600 via-transparent   to-slate-600 bg-[length:400%_400%] bg-center  hover:bg-[#ffffff] hover:transition-all hover:duration-300 mt-5">
-                    <p className="text-[#47507A] font-semibold text-xl flex mr-2">Sort by</p>
-                    <img src={Arrow} className="" />
+                <button className="lg:w-[150px] lg:h-[54px] md:w-[120px] md:h-[43px] sm:w-[100px] sm:h-[37px] xs:w-[100px] sm:h-[37px] border-[#282D45] border-2 rounded-[46px] flex justify-center items-center bg-gradient-to-r from-slate-600 via-transparent   to-slate-600 bg-[length:400%_400%] bg-center  hover:bg-[#ffffff] hover:transition-all hover:duration-300 mt-5 p-2">
+                    <p className="text-[#47507A] font-semibold lg:text-xl md:text-base sm:text-sm xs:text-xs flex mr-2 ">Sort by</p>
+                    <img src={Arrow} className="lg:w-1/5 md:w-1/6 sm:w-[14%] xs:w-[12.5%]" />
                 </button>
             </div>
         </div>
