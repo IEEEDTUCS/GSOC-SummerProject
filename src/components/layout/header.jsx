2f2a7ba50e5b3d8da4982d4a3c2473234e28c1f3
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-07-15 08:18:20 +0530

    Added Header and Footer Components

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,2 +1,23 @@
 // header banana hai as a component and then make it position fixed  on top
 // comopent name - <Header> 
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='fixed top-0 left-0 right-0 text-white flex justify-around items-center py-4 border-box border-b-[1px] border-gray-800'>
+        <div>
+            <img src="src/assets/logo.svg" alt="logo" />
+        </div>
+        <div className='flex justify-between gap-8 text-gray-400'>
+            <div className='text-white'>Home</div>
+            <div>Contributors</div>
+            <div>Top Companies</div>
+            <div>Contact Us</div>
+            <div>Our Team</div>
+        </div>
+        <div className='bg-violet-600 py-2 px-4 rounded-full'>Dashboard</div>
+    </div>
+  )
+}
+
+export default Header
