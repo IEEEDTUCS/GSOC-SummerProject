619a30c8ba7bee5cd8db37614a57779e82dfaded
TarushS <tsonakya@gmail.com>
2024-07-16 04:19:20 +0530

    chore: home card work started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import HomeCard from "./components/HomeCard";
 
 function App() {
   const a = 23;
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">IEEE Summer of Code</h1>
+      <div className="flex justify-center items-center">
+        <HomeCard/>
+      </div>
     </>
   );
 }
---
+++ src/components/HomeCard.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Button } from "./ui/button"
+
+
+const HomeCard = () => {
+  return (
+    <div className="flex w-[806px] h-[444px] border-2 rounded-lg flex-col overflow-hidden items-center justify-center" style={{ borderColor: '#282D45', backgroundColor: '#0e1330' }}>
+      <div className="flex w-full h-[29px] items-center" style={{ backgroundColor: '#282D45' }}>
+        <div className="flex space-x-2" style={{ marginLeft: '15px' }}>
+          <div className="w-3 h-3 rounded-full" style={{ backgroundColor: '#0e1330' }}></div>
+          <div className="w-3 h-3 rounded-full" style={{ backgroundColor: '#0e1330' }}></div>
+          <div className="w-3 h-3 rounded-full" style={{ backgroundColor: '#0e1330' }}></div>
+        </div>
+      </div>
+      <div className='flex w-[808px] h-[437px] justify-center items-center'>
+
+      <HomeGraph/>
+
+      </div>
+    </div>
+  );
+}
+
+const HomeGraph = () => {
+  return (
+    <div className='flex w-full h-full mx-10'>
+      <div className='flex w-full my-9'>
+        <div id='Avatar_bar' className='flex w-full h-12'>
+          <Avatar className='h-12 w-12'>
+            <AvatarImage src="https://github.com/tarushs.png" />
+            <AvatarFallback>TS</AvatarFallback>
+          </Avatar>
+          <div className='h-12 w-96 flex flex-col items-start ml-4'>
+            <text className='font-semibold' style={{ fontFamily: 'satoshi', color: '#ffffff', fontSize: '20px'}}>Company Name</text>
+            <text className='font-normal' style={{ color: '#8F9BB7', fontSize: '14px'}}>The 3D Toolkit provides algorithms and methods..</text>
+          </div>
+          <div className='ml-auto self-center '><Button className='border-gray-600 border-2 rounded text-white' variant="outline" >Visit Site</Button></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeCard;
