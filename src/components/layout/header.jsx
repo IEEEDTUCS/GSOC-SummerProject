7444ffbad4e55acd6d441b5c22d6a75183ad9043
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-07-16 12:46:22 +0530

    Added white color on click functionality

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,19 +1,24 @@
 // header banana hai as a component and then make it position fixed  on top
 // comopent name - <Header> 
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [isActive, setIsActive] = useState('Home')
+
+  const handleClick = (element) => {
+    setIsActive(element)
+  }
   return (
     <div className='fixed top-0 left-0 right-0 text-white flex justify-around items-center py-4 border-box border-b-[1px] border-gray-800'>
         <div>
             <img src="src/assets/logo.svg" alt="logo" />
         </div>
-        <div className='flex justify-between gap-8 text-gray-400'>
-            <div className='text-white'>Home</div>
-            <div>Contributors</div>
-            <div>Top Companies</div>
-            <div>Contact Us</div>
-            <div>Our Team</div>
+        <div className='flex justify-between gap-8'>
+            <button onClick={() => handleClick('Home')} className={isActive==='Home'? 'text-white':'text-gray-400'}>Home</button>
+            <button onClick={() => handleClick('Contributors')} className={isActive==='Contributors'? 'text-white':'text-gray-400'}>Contributors</button>
+            <button onClick={() => handleClick('Top Companies')} className={isActive==='Top Companies'? 'text-white':'text-gray-400'}>Top Companies</button>
+            <button onClick={() => handleClick('Contact Us')} className={isActive==='Contact Us'? 'text-white':'text-gray-400'}>Contact Us</button>
+            <button onClick={() => handleClick('Our Team')} className={isActive==='Our Team'? 'text-white':'text-gray-400'}>Our Team</button>
         </div>
         <div className='bg-violet-600 py-2 px-4 rounded-full'>Dashboard</div>
     </div>
