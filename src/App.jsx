5589396374dd94549f3d1334bb1da9bfe92ec7bc
Sangam Koundal <koundalsangam314@gmail.com>
2024-07-19 21:31:10 +0530

    fetched the contents of the API to the files and used them

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import TopCompany from "./components/top-company/page.top-company";
-import CompanyBox from "./components/Company-List/company.box.component";
-import Filter from "./components/Company-List/Filter";
-import CompanyList from "./components/Company-List/page.company";
+import TopCompany from "./_components/top-company/page.top-company";
+import CompanyBox from "./_components/Company-List/company.box.component";
+import Filter from "./_components/Company-List/Filter";
+import CompanyList from "./_components/Company-List/page.company";
 function App() {
   return (
   // <TopCompany />
