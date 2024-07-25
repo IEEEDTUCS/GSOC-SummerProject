9d3f2179bc317297b3502b288bb8a506e79f6eb1
Sangam Koundal <koundalsangam314@gmail.com>
2024-07-25 20:23:18 +0530

    Made the top-companies page,search page,Comapany card and popup filter

--- src/(pages)/top-companies/page.jsx
+++ src/(pages)/top-companies/page.jsx
@@ -1,1 +1,18 @@
-// isme mostly chije paste karne wali hai isliye isse algi meet ke rakh lete hai
+import React, { useState, useEffect } from "react";
+
+// Main Component
+export default function CompanyList(){
+
+  return (
+    <div >
+      <div className="flex flex-col items-center justify-center mb-20">
+      <p className="lg:text-6xl md:text-5xl sm:text-4xl xs:text-3xl  font-semibold  inline-block text-transparent bg-clip-text bg-gradient-to-b from-white via-slate-200 to-slate-600 mt-3">Top Companies</p>
+      
+      <button className="lg:w-[150px] lg:h-[54px] md:w-[120px] md:h-[43px] sm:w-[100px] sm:h-[37px] xs:w-[100px] xs:h-[37px] border-[#282D45] border-2 rounded-[46px] flex justify-center items-center bg-gradient-to-r from-slate-600 via-transparent   to-slate-600 bg-[length:400%_400%] bg-center  hover:bg-[#ffffff] hover:transition-all hover:duration-300 mt-5 p-2">
+                <p className="text-[#47507A] font-semibold lg:text-xl md:text-base sm:text-sm xs:text-xs flex mr-2 ">Sort by</p>
+                     <img src="src/assets/top-companies-arrow.png" className="lg:w-1/5 md:w-1/6 sm:w-[14%] xs:w-[12.5%]" />
+                 </button>
+      </div>
+    </div>
+  );
+};
--- src/_components/CompanyCard/CompanyCard.jsx
+++ src/_components/CompanyCard/CompanyCard.jsx
@@ -1,3 +1,47 @@
-// isme comapny card wala component aayega 
-// use props for company name, desc, and other features, sath me isse clickable banana hai  jo ki  redirect kare dashboard pe
+import React from "react";
 
+export default function CompanyCard(image,heading,description,tags,clan){
+    return(
+        
+
+<div class="max-w-sm  border border-[#282D45] rounded-lg shadow bg-[#0E1330] dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg p-5" src={image} alt="" />
+    </a>
+    <div class="px-5 pb-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{heading}</h5>
+        </a>
+        <p class=" font-normal text-[#8F9BB7] dark:text-gray-400">{description}</p>
+        <hr class="h-px my-5 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        
+
+        {/* tags */}
+        <div className="flex gap-3 w-auto">
+        {tags.map((tag, index) => (
+            <button
+              key={index}
+              className="text-[#41E88D] px-1 rounded-3xl bg-gradient-to-r from-[rgba(183,255,196,0.2)] to-[rgba(183,255,196,0.2)] bg-[length:300%_300%] bg-center"
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+
+        <div className="flex justify-between items-center">
+            <p className="text-[#8F9BB7]">{clan}</p>
+            <a href="#" class="inline-flex items-center px-3 py-2 text-xl text-center text-white ">
+                Analysis
+                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+        
+    </div>
+</div>
+
+    );
+}
+
+// to-slate-200
