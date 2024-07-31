4d1967e56e0d8ab27a7586da52721f2f41ab35f0
Unnat Agrawal <agrawalunnat7779@gmail.com>
2024-07-31 23:18:32 +0530

    our-team-page-ready

--- src/(pages)/our-team/page.jsx
+++ src/(pages)/our-team/page.jsx
@@ -1,1 +1,246 @@
 // jo card banaya  hai usse yaha show kar diyo 
+import React from 'react'
+import MemberCard from "@components/MemberCard/MemberCard";
+
+const arr = [
+  {
+    name:"Unnat",
+    
+    image:"\img1.jpg",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.youtube.com",
+    github:"https://www.google.com"
+  },
+  {
+    name:"Abhimanyu",
+    image:"\img2.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+    
+    
+  },
+  {
+    name:"Tasneem",
+    image:"\img3.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+  },
+  {
+
+    name:"Unnat",
+    image:"\img1.jpg",
+    linkedin:"www.linkedin.com/in/unnat7779",
+    insta:"instagram.com/_unnat_7779",
+    mail:"mailto:agrawalunnat7779@gmail.com",
+    github:"https://github.com/unnat7779"
+
+  },
+  {
+    name:"Aman",
+    image:"\Image Placeholder.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+    
+    
+  },
+  {
+    name:"Aman",
+    image:"\Image Placeholder.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+  },
+  {
+
+    name:"Aman",
+    image:"\Image Placeholder.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+
+  },
+  {
+    name:"Aman",
+    image:"\Image Placeholder.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+    
+    
+  },
+  {
+    name:"Aman",
+    image:"\Image Placeholder.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+  },
+  {
+
+    name:"Aman",
+    image:"\Image Placeholder.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+
+  },
+  {
+    name:"Aman",
+    image:"\Image Placeholder.png",
+    linkedin:"www.linkedin.com/in/unnatagrawal",
+    insta:"https://www.google.com",
+    mail:"https://www.google.com",
+    github:"https://www.google.com"
+    
+    
+  }
+  
+]
+const Page = () => {
+  return (
+    <div > 
+      <div>
+      <h1 className=' mt-20 text-[60px] font-semibold bg-gradient-to-t from-[#7E808F] to-[#F6F6F7] bg-clip-text text-transparent' >IEEE DTU Computer Society </h1><br />
+      </div>
+      
+      <div className=' flex flex-wrap gap-y-7 sm:flex gap-x-7 justify-center'>
+        <div>
+        <MemberCard
+              name={arr[0].name}
+              image={arr[0].image}
+              insta={arr[0].insta}
+              linkedin={arr[0].linkedin}
+              github={arr[0].github}
+              mail={arr[0].mail}
+            />
+        </div>
+        <div>
+        <MemberCard
+              name={arr[1].name}
+              image={arr[1].image}
+              insta={arr[1].insta}
+              linkedin={arr[1].linkedin}
+              github={arr[1].github}
+              mail={arr[1].mail}
+            />
+        </div>
+        <div>
+        <MemberCard
+              name={arr[2].name}
+              image={arr[2].image}
+              insta={arr[2].insta}
+              linkedin={arr[2].linkedin}
+              github={arr[2].github}
+              mail={arr[2].mail}
+            />
+        </div>
+      </div>
+      <div>
+      <h2 className=' mt-10 text-[60px] font-semibold bg-gradient-to-t from-[#7E808F] to-[#F6F6F7] bg-clip-text text-transparent' > Contributors </h2><br />
+      </div>
+      <div className=' flex flex-wrap gap-y-7 sm:flex gap-x-7 mx-0 justify-center' >
+        <div  >
+        <MemberCard
+              name={arr[3].name}
+              image={arr[3].image}
+              insta={arr[3].insta}
+              linkedin={arr[3].linkedin}
+              github={arr[3].github}
+              mail={arr[3].mail}
+            />
+        </div>
+        <div><MemberCard
+              name={arr[4].name}
+              image={arr[4].image}
+              insta={arr[4].insta}
+              linkedin={arr[4].linkedin}
+              github={arr[4].github}
+              mail={arr[4].mail}
+            /></div>
+        <div>
+        <MemberCard
+              name={arr[5].name}
+              image={arr[5].image}
+              insta={arr[5].insta}
+              linkedin={arr[5].linkedin}
+              github={arr[5].github}
+              mail={arr[5].mail}
+            />
+        </div>
+        <div>
+        <MemberCard
+              name={arr[6].name}
+              image={arr[6].image}
+              insta={arr[6].insta}
+              linkedin={arr[6].linkedin}
+              github={arr[6].github}
+              mail={arr[6].mail}
+            />
+        </div>
+        <div>
+        <MemberCard
+              name={arr[7].name}
+              image={arr[7].image}
+              insta={arr[7].insta}
+              linkedin={arr[7].linkedin}
+              github={arr[7].github}
+              mail={arr[7].mail}
+            />
+        </div>
+        <div>
+        <MemberCard
+              name={arr[8].name}
+              image={arr[8].image}
+              insta={arr[8].insta}
+              linkedin={arr[8].linkedin}
+              github={arr[8].github}
+              mail={arr[8].mail}
+            />
+        </div>
+        <div>
+        <MemberCard
+              name={arr[9].name}
+              image={arr[9].image}
+              insta={arr[9].insta}
+              linkedin={arr[9].linkedin}
+              github={arr[9].github}
+              mail={arr[9].mail}
+            />
+        </div>
+        <div>
+        <MemberCard
+              name={arr[10].name}
+              image={arr[10].image}
+              insta={arr[10].insta}
+              linkedin={arr[10].linkedin}
+              github={arr[10].github}
+              mail={arr[10].mail}
+            />
+        </div>
+        
+      </div>
+      {/* <div className='flex flex-wrap gap-y-7 sm: grid grid-cols-3  gap-20  mt-10' >
+        <div><MemberCard/></div>
+        <div><MemberCard/></div>
+        <div><MemberCard/></div>
+        <div><MemberCard/></div>
+        
+      </div> */}
+
+    </div>
+  )
+}
+
+export default Page
