e0a878d735883f811e78ed7813ca937b84a4b164
Unnat Agrawal <agrawalunnat7779@gmail.com>
2024-08-02 03:44:00 +0530

    mapping done

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '@pages/home/page';
 import Dashboard from '@pages/dashboard/page';
-<<<<<<< Updated upstream
+// Updated upstream
 import TopCompanies from "./(pages)/top-companies/page";
 import Header from '@components/layout/header';
 import Footer from '@components/layout/footer';
 import Search  from '@pages/search/page';
 import Filter from '@components/PopupFilter/Filter';
 import CompanyCard from '@components/CompanyCard/CompanyCard';
-=======
 import MemberCard from '@components/MemberCard/MemberCard';
 import Page from '@pages/our-team/page';
->>>>>>> Stashed changes
+// Stashed changes
 
 
 function AppRouter() {
